549788d3ee6246912a483fec0ac2b087820d4493
dhbryan75 <dhbryan75@gmail.com>
2023-07-03 18:39:57 +0900

    도현 L1

---
+++ L1/kdh.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    return 0;
+}
+
+// O(n^2)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i=0; i<nums.size(); i++) {
+            for(int j=i+1; j<nums.size(); j++) {
+                if(nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
