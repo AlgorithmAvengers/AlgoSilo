b61f0e3be364fd261b671e14d5809acab2e5b87b
dhbryan75 <dhbryan75@gmail.com>
2023-08-14 18:06:02 +0900

    도현 6주차 풀이 개선

--- B1915/kdh.cpp
+++ B1915/kdh.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 int n, m;
 string s;
-int row[1005][1005] = {};
-int col[1005][1005] = {};
 int box[1005][1005] = {};
 int maxLen = 0;
 
@@ -16,9 +14,7 @@ int main() {
             if(s[j-1] == '0') {
                 continue;
             }
-            row[i][j] = row[i][j-1] + 1;
-            col[i][j] = col[i-1][j] + 1;
-            box[i][j] = min(box[i-1][j-1], min(row[i][j-1], col[i-1][j])) + 1;
+            box[i][j] = min(box[i-1][j-1], min(box[i][j-1], box[i-1][j])) + 1;
             if(box[i][j] > maxLen) {
                 maxLen = box[i][j];
             }
